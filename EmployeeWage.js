ea80f88d8d9f9fd66f3561d2895ad57e72c05fbe
varun <varunlad5@gmail.com>
2021-10-22 18:15:27 +0530

    [Varun] Check Wether Employee is Present or Absent

---
+++ EmployeeWage.js
@@ -0,0 +1,10 @@
+//UC1
+const IS_ABSENT=0;
+let employeeCheck=Math.floor(Math.random()*2);
+if(employeeCheck==IS_ABSENT)
+    console.log("UC1 Employee is Absent")
+else
+    console.log("UC1 Employee is Present")
+
+
+
