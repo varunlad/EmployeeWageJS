e193c2181f912df735b13e4fdb028793243bd014
varun <varunlad5@gmail.com>
2021-10-22 19:41:22 +0530

    [Varun] Calculating Total Wage in 20 Days or in 120 hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,6 +5,7 @@ const PART_TIME_Hours=4;
 const FULL_TIME_Hours=8;
 const RATE_PER_HOUR=100;
 const MAX_NUMBER_DAYS=20;
+const MAX_HOURS=160;
 let empHours=0;
 function getEmpHours(EmployeeCheck)
 {
@@ -20,13 +21,13 @@ switch(EmployeeCheck)
 }
 let days=0;
 let maxHours=0;
-for(day=0;days<MAX_NUMBER_DAYS ;days++)
+while(days<MAX_NUMBER_DAYS && empHours<=160)
 {
 let EmployeeCheck=Math.floor(Math.random()*3);
 empHours+=getEmpHours(EmployeeCheck);
-
+days=days+1;
 }
 let empWage=empHours*RATE_PER_HOUR;
-console.log("UC 4 Total Employee Wage of the day is :"+empWage);
+console.log("UC 5 Total Employee Wage in  "+empHours +"Hours "+" Working for"+days+" days "+"is :"+empWage);
 
 
