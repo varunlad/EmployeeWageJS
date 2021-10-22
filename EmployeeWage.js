e574cceebdb390d67452f9c5346de125db9ba260
varun <varunlad5@gmail.com>
2021-10-22 18:26:34 +0530

    [Varun] Daily Wage of Employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,27 @@
-//UC1
-const IS_ABSENT=0;
-let employeeCheck=Math.floor(Math.random()*2);
-if(employeeCheck==IS_ABSENT)
-    console.log("UC1 Employee is Absent")
-else
-    console.log("UC1 Employee is Present")
-
+//UC2
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_Hours=4;
+const FULL_TIME_Hours=8;
+const RATE_PER_HOUR=100;
+let empHours=0;
+let EmployeeCheck=Math.floor(Math.random()*3);
+switch(EmployeeCheck)
+{
+   case 1:
+   empHours=PART_TIME_Hours; 
+   console.log("UC 2 Employee is Present Part Time");
+   break;
+   case 2:
+   empHours=FULL_TIME_Hours;
+   console.log("UC 2 Employee is Present Full Time");
+   break;
+   default :
+   empHours=0;
+   console.log("Employee is Absent")
+   break;    
+}
+let empWage=empHours*RATE_PER_HOUR;
+console.log("UC 2 the Employee Wage of the day is :"+empWage);
 
 
