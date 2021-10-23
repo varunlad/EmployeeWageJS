4c00c950ab76dee224fc5c2df0f05150b9d823ce
varun <varunlad5@gmail.com>
2021-10-23 09:57:16 +0530

    [Varun] Calculating Total Wage and storing in array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,7 +6,13 @@ const FULL_TIME_Hours=8;
 const RATE_PER_HOUR=100;
 const MAX_NUMBER_DAYS=20;
 const MAX_HOURS=160;
+let totalEmpHours=0;
 let empHours=0;
+let empDailyWageArr=new Array();
+function CalculateDailyWage(empHours)
+{
+   return empHours*RATE_PER_HOUR
+}
 function getEmpHours(EmployeeCheck)
 {
 switch(EmployeeCheck)
@@ -23,11 +29,13 @@ let days=0;
 let maxHours=0;
 while(days<MAX_NUMBER_DAYS && empHours<=160)
 {
-let EmployeeCheck=Math.floor(Math.random()*3);
-empHours+=getEmpHours(EmployeeCheck);
 days=days+1;
+let EmployeeCheck=Math.floor(Math.random()*10)%3;
+empHours=getEmpHours(EmployeeCheck);
+totalEmpHours+=empHours;
+empDailyWageArr.push(CalculateDailyWage(empHours))
 }
-let empWage=empHours*RATE_PER_HOUR;
-console.log("UC 5 Total Employee Wage in  "+empHours +"Hours "+" Working for"+days+" days "+"is :"+empWage);
+let empWage=CalculateDailyWage(totalEmpHours)
+console.log("UC 6 Total Employee Wage in  "+totalEmpHours +"Hours "+" Working for"+days+" days "+"is :"+empWage);
 
 
